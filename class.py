aafa50f42c992892c1568e31ea3c332c5ccae119
nishajasmin <nishajasmin.c@gmail.com>
2021-10-21 19:28:34 +0530

    my first commit

---
+++ class.py
@@ -0,0 +1,17 @@
+class student:
+    def __init__(self,name,age,place):
+        self.name=name
+        self.age=age
+        self.place=place
+    def display(self):
+        print("name :"+self.name)
+        print("age :"+self.age)
+        print("place :"+self.place)
+
+s1=student("nisha","19","lakkidi")
+s1.display()
+
+
+
+
+
